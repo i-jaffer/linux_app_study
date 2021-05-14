563825192b5f5d2d6e608ebce9838ffbdc319f9b
wl_ubuntu_mynotebook <crazywl666666@gmail.com>
2021-05-14 10:26:23 +0800

    [fix]fix signchild part of the bug
    Signed-off-by: wl_ubuntu_mynotebook <crazywl666666@gmail.com>

--- signal/signchild.c
+++ signal/signchild.c
@@ -24,6 +24,12 @@ int main()
 	pid_t pid;
 	int i = 0;
 
+	/* shield SIGCHLD signal */
+	sigset_t new_set,old_set;
+	sigemptyset(&new_set);
+	sigaddset(&new_set,SIGCHLD);
+	sigprocmask(SIG_BLOCK,&new_set,&old_set);
+
 	for(i=0; i<10; i++) {
 		pid = fork();
 		if(pid == -1) {
@@ -36,23 +42,18 @@ int main()
 	if(i < 10) {
 		sleep(1);
 		printf("this is %dth child,pid:%d\n",i,getpid());
+		return i;
 	} else {
-		/* shield SIGCHLD signal */
-		sigset_t new_set,old_set;
-		sigemptyset(&new_set);
-		sigaddset(&new_set,SIGCHLD);
-		sigprocmask(SIG_BLOCK,&new_set,&old_set);
-		
 		/* register SIGCHLD signal call back */
 		struct sigaction new_act,old_act;
 		new_act.sa_handler = sig_catch_cb;
 		sigemptyset(&(new_act.sa_mask));
 		new_act.sa_flags = 0;
 		sigaction(SIGCHLD, &new_act, &old_act);
-		
+
 		/* cancel shield SIGCHLD signal */
 		sigprocmask(SIG_SETMASK,&old_set,NULL);
-		
+
 		while(1);
 	}
 
